69e19ecdb9feba9a228aecf33da34580c880a890
Abhishek Purty <abhishekpurty1999@gmail.com>
2024-01-18 11:35:08 +0530

    carousel update

--- components/Testmonial_v2.tsx
+++ components/Testmonial_v2.tsx
@@ -26,7 +26,7 @@ const Testmonial_v2 = () => {
       // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
-      slidesPerView={3}
+      slidesPerView={1}
       navigation
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
